b8b6ac793d82e2d3588137a7f08c70683e7442eb
SoYoung210 <ethdud1@gamil.com>
2018-07-14 19:56:19 +0900

    # DataFlow with Props Code

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -3,17 +3,23 @@ import './Movie.css';
 
 class Movie extends Component {
 	render() {
+		console.log(this.props);
 		return ( 
-		  <h1>hello this is a movie</h1>
+		<div>
+			<MoviePoster poster={this.props.poster}/>
+			{/* JSX 는 명령을 수행하려면 괄호를 쳐야해. */}
+			<h1>{this.props.title}</h1>
+		</div>
 		)
 	}
 }
 
 class MoviePoster extends Component {
 	render() {
+		console.log(this.props);
 		return (
 			//dummy 이미지 셋팅하기.
-			<img src = ""/>
+			<img src = {this.props.poster} />
 		)
 	}
 }
